34cd74652415aabfed6f624650bd46eae7050f89
DanieLuan <danielluanlourencol@gmail.com>
2022-02-11 16:09:17 -0300

    organização e make exec

---
+++ src/definitions.h
@@ -0,0 +1,14 @@
+//Arquivo com as definições. TYPEDEF or DEFINE
+#ifndef DEF
+#define DEF
+
+#define MAX_STR 50
+#define MAX_WEIGHT 10
+
+  typedef struct {
+    int X;
+    int Y;
+    char botID[MAX_STR];
+} bot_info;
+
+#endif
